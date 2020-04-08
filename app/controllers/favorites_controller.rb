b10cccb2acd52d503b2bb3969f973998591a97e3
naoya-sumi0613 <s.naoya613pcstudy@gmail.com>
2020-04-08 15:34:10 +0900

    [Add]いいね機能非同期(途中)

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,11 @@ class FavoritesController < ApplicationController
 		@books = Book.find(params[:book_id])
 		favorite = current_user.favorites.new(book_id: @books.id)
 		favorite.save
-		redirect_back(fallback_location:root_path)
 	end
 
 	def destroy
 		@books = Book.find(params[:book_id])
 		favorite = current_user.favorites.find_by(book_id: @books.id)
 		favorite.destroy
-		redirect_back(fallback_location:root_path)
 	end
 end
